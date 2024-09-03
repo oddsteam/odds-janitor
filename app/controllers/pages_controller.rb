c8257aefe2e14f4507cf6acadb67bb769088157f
pixel <pixel@odds.team>
2024-09-03 17:09:59 +0700

    chore: Update pagesController to display meeting rooms in home view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,63 @@
 class PagesController < ApplicationController
   def home
+    @rooms = [
+      {
+        id: 1,
+        room_name: "Meeting 1",
+        room_address: "Binary Base",
+        seat: 3,
+        room_description: "Small meeting room",
+      },
+      {
+        id: 2,
+        room_name: "Meeting 2",
+        room_address: "Binary Base",
+        seat: 6,
+        room_description: "Medium meeting room",
+      },
+      {
+        id: 3,
+        room_name: "Territory 1",
+        room_address: "Binary Base",
+        seat: 5,
+        room_description: "Large meeting room",
+      },
+      {
+        id: 4,
+        room_name: "Territory 2",
+        room_address: "Binary Base",
+        seat: 5,
+        room_description: "Extra large meeting room",
+      },
+      {
+        id: 5,
+        room_name: "Territory 3",
+        room_address: "Binary Base",
+        seat: 5,
+        room_description: "Extra large meeting room",
+      },
+      {
+        id: 6,
+        room_name: "Global",
+        room_address: "Binary Base",
+        seat: 30,
+        room_description: "Extra large meeting room",
+      },
+      {
+        id: 7,
+        room_name: "All Nighter 1",
+        room_address: "Binary Base",
+        seat: 36,
+        room_description: "LeSSex Area",
+      },
+      {
+        id: 8,
+        room_name: "All Nighter 2",
+        room_address: "Binary Base",
+        seat: 32,
+        room_description: "LeSSex Area",
+      },
+    ];
   end
 
   def test
