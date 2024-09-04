bbe4bab36cd6016c7c3e1028ec61d4c79f36322c
pixel <pixel@odds.team>
2024-09-04 13:12:35 +0700

    feat: Move booking table to /reserves

--- app/controllers/reserves_controller.rb
+++ app/controllers/reserves_controller.rb
@@ -4,6 +4,66 @@ class ReservesController < ApplicationController
   # GET /reserves or /reserves.json
   def index
     @reserves = Reserve.all
+    @user = Page.get_user_form_session(session)
+
+    @rooms = [
+      {
+        id: 1,
+        room_name: "Meeting 1",
+        room_address: "Binary Base",
+        seat: 3,
+        room_description: "Small meeting room",
+      },
+      {
+        id: 2,
+        room_name: "Meeting 2",
+        room_address: "Binary Base",
+        seat: 6,
+        room_description: "Medium meeting room",
+      },
+      {
+        id: 3,
+        room_name: "Territory 1",
+        room_address: "Binary Base",
+        seat: 5,
+        room_description: "Large meeting room",
+      },
+      {
+        id: 4,
+        room_name: "Territory 2",
+        room_address: "Binary Base",
+        seat: 5,
+        room_description: "Extra large meeting room",
+      },
+      {
+        id: 5,
+        room_name: "Territory 3",
+        room_address: "Binary Base",
+        seat: 5,
+        room_description: "Extra large meeting room",
+      },
+      {
+        id: 6,
+        room_name: "Global",
+        room_address: "Binary Base",
+        seat: 30,
+        room_description: "Extra large meeting room",
+      },
+      {
+        id: 7,
+        room_name: "All Nighter 1",
+        room_address: "Binary Base",
+        seat: 36,
+        room_description: "LeSSex Area",
+      },
+      {
+        id: 8,
+        room_name: "All Nighter 2",
+        room_address: "Binary Base",
+        seat: 32,
+        room_description: "LeSSex Area",
+      },
+    ]
   end
 
   # GET /reserves/1 or /reserves/1.json
