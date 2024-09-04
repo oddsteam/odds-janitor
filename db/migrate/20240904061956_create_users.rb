b814fd0131a05d4515e9e64afe0abfe2b21be574
e3tow1c <thepphithak.w@kkumail.com>
2024-09-04 13:23:50 +0700

    make activeRecord default id: false, change user_id type to string and add primary_key true property to user_id attribute

--- db/migrate/20240904061956_create_users.rb
+++ db/migrate/20240904061956_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
-    create_table :users do |t|
-      t.primary_key :user_id
+    create_table :users, id: false do |t|
+      t.string :user_id, primary_key: true
       t.string :name
       t.string :email
 
