1ba64807785779ffde8ac82d42ce6e1194ff39b1
dew <natthaphong.t@kkumail.com>
2024-09-05 13:53:08 +0700

    Refactor: font size and fixed size for All Night 1 in blueprint map

--- app/controllers/roomdetails_controller.rb
+++ app/controllers/roomdetails_controller.rb
@@ -22,7 +22,7 @@ class RoomdetailsController < ApplicationController
       {id: 4, name: "TERRITORY 2", x: 169, y: 554.5, width: 184.7, height: 164.5},
       {id: 5, name: "TERRITORY 3", x: 93.5, y: 352.5, width: 260, height: 186.5},
       {id: 6, name: "GLOBAL", x: 93.5, y: 55.5, width: 669.5, height: 281},
-      {id: 7, name: "ALL NIGHTER 1", x: 1805, y: 535, width: 570, height: 279.5},
+      {id: 7, name: "ALL NIGHTER 1", x: 1805, y: 418.05, width: 570, height: 500},
       {id: 8, name: "ALL NIGHTER 2", x: 1805, y: 54, width: 675, height: 406.5}
     ]
   end
