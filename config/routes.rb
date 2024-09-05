e387d9c0229241294af8f2cea8091d6e624c2254
pixel <pixel@odds.team>
2024-09-05 17:16:01 +0700

    refactor: set reserves page to root and refactor nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,9 @@ Rails.application.routes.draw do
   get 'pages/home'
   
   get "up" => "rails/health#show", as: :rails_health_check
-  root "pages#home"
   get "/lobby", to: "sessions#lobby"
   get "/callback", to: "sessions#callback"
   get "logout", to: "sessions#logout"
+
+  root "reserves#index"
 end
