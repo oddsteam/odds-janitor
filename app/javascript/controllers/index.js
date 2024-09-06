ea2aea1ddfe65209a64a10c8404767528aec811e
wisanu <wisanu@odds.team>
2024-09-06 12:57:18 +0700

    Change name file boking controller to reserves controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,8 +2,8 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
-import BookingTableController from "./booking_table_controller" // ชื่อไฟล์และตัวแปรตรงนี้ต้องตรงกัน
-application.register("booking-table", BookingTableController) // ตรวจสอบชื่อว่าเป็น booking-table
+import ReservesController from "./reserves_controller"
+application.register("reserves", ReservesController)
 
 // import CalendarController from "./calendar_controller"
 // application.register("calendar", CalendarController)
