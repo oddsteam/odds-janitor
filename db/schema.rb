fc353fb2cb6dd79c33283cdebd1b5a2c4f1e8377
Nonnicha <non@odds.team>
2024-09-23 13:45:22 +0700

    Change credentials for can run project

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_09_06_025525) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "reserves", force: :cascade do |t|
     t.string "date"
     t.string "start_timer"
